4ad5eabf76f64ac7937fa24e325d5cf5a3f70310
ilnur543623476457 <126465618+ilnur543623476457@users.noreply.github.com>
2025-02-18 15:52:41 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
 window.addEventListener('load', () => {
-    const tg = window.Telegram.WebApp;
-    tg.expand();
     const user_id = tg.initDataUnsafe.query_id
     var elem = document.getElementById('name');
     // elem.innerHTML = user_id
